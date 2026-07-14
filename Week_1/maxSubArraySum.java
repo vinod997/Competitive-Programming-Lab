7a926ffae45c490515d406ddc6fec5dba9bdcc09
Vinod Yalamanchi <yalamanchivinod20@gmail.com>
2026-07-14 11:52:47 +0530

    Initial commit

---
+++ Week_1/maxSubArraySum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class maxSubArraySum {
+    public int maxSubArray(int[] nums) {
+        int max=Integer.MIN_VALUE,n=nums.length;
+        for(int i=0;i<nums.length;i++){ 
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                if(sum > max) max=sum;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements of the array: ");
+        int[] nums = new int[n];
+        for(int i=0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+        maxSubArraySum obj = new maxSubArraySum();
+        System.out.println(obj.maxSubArray(nums));
+    }
+}
